0f6aca12f63fdc9aa4dcf1c18c6015907716fa1f
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-30 09:25:14 -0800

    Imported Animal.css into Animal.js

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,6 +1,8 @@
+import './Animal.css';
+
 export default function Animal(props) {
   return (
-    <div className="animals" style={{ left: `${props.left}`, top: `${props.top}` }}>
+    <div className="animal" style={{ left: `${props.left}`, top: `${props.top}` }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       <h2>{props.name}</h2>
       <h4>{props.says}</h4>
