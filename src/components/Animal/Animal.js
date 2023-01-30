476a2d8d74d1d26063253d13829a778fdaa758ff
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-30 09:21:04 -0800

    Animals displaying

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animals" style={{ left: `${props.left}`, top: `${props.top}` }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <h2>{props.name}</h2>
+      <h4>{props.says}</h4>
+    </div>
+  );
 }
